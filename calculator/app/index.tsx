42e67fcb16678ea480a9548c0e86c719a982ad66
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-09-04 13:11:03 -0600

    fix: remove unnecessary folders
    remove folders from original installation

---
+++ calculator/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      {" "}
+      <Text>calculator 2</Text>{" "}
+    </View>
+  );
+};
+
+export default CalculatorApp;
